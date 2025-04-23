83c40df6fdf644d51b545ecd33eee02ad604429e
webdevmahabub <mahbubbd926@gmail.com>
2025-04-23 18:44:49 +0600

    Show Module and Lesson Actual Data in Sidebar Part 2

--- app/(main)/courses/[id]/lesson/_components/course-sidebar.jsx
+++ app/(main)/courses/[id]/lesson/_components/course-sidebar.jsx
@@ -1,27 +1,30 @@
 "use client";
 import { useState } from "react";
 import { CourseProgress } from "@/components/course-progress";
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion";
-import { Button } from "@/components/ui/button";
-import { cn } from "@/lib/utils";
-import { CheckCircle } from "lucide-react";
-import { PlayCircle } from "lucide-react";
-import { Lock } from "lucide-react";
-import Link from "next/link";
-import { ReviewModal } from "./review-modal";
-import { DownloadCertificate } from "./download-certificate";
-import { GiveReview } from "./give-review";
-import { SidebarModules } from "./sidebar-modules";
 
-export const CourseSidebar = () => {
+
+
+export const CourseSidebar = async ({courseId}) => {
+
+  const course = await getCourseDetails(courseId);
+  const loggedinUser = await getLoggedInUser();
+
+  const updatedataModules = await Promise.all(course?.modules.map(async(module) => {
+    const moduleId = module._id.toString();
+    const lessons = module?.lessonIds;
+
+  const updatedLessons = await Promise.all(lessons.map(async (lesson) => {
+    const lessonId = lesson._id.toString();
+    
+  }))
+
+
+  }))
+
   const [isReviewModalOpen, setIsReviewModalOpen] = useState(false);
   const isActive = true;
   const isCompleted = true;
+  
   return (
     <>
       <div className="h-full border-r flex flex-col overflow-y-auto shadow-sm">
--- app/(main)/courses/[id]/lesson/layout.jsx
+++ app/(main)/courses/[id]/lesson/layout.jsx
@@ -26,7 +26,7 @@ const CourseLayout = async ({ children,params: {id} }) => {
       <div className="h-[80px] lg:pl-96 fixed top-[60px] inset-y-0 w-full z-10">
       <div className="flex lg:hidden p-4 border-b h-full items-center bg-white shadow-sm relative">
           {/* Course Sidebar For Mobile */}
-          <CourseSidebarMobile />
+          <CourseSidebarMobile courseId={id} />
           {/* <NavbarRoutes /> */}
         </div>
       </div>
@@ -34,7 +34,7 @@ const CourseLayout = async ({ children,params: {id} }) => {
       <div className="grid grid-cols-1 lg:grid-cols-12">
       <div className="hidden lg:flex h-full w-96 flex-col inset-y-0 z-50">
         {/* sidebar starts */}
-        <CourseSidebar />
+        <CourseSidebar courseId={id} />
         {/* sidebar ends */}
       </div>
       <main className="lg:pl-96 pt-[80px] lg:pt-[20px] h-full col-span-10 px-4">{children}</main>
